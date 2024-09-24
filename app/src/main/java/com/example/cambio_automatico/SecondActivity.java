deecebaca7b0509ad35caabe268aa7d84092d36e
Carlos Jesus Quintero Aldana <yeshuajriv12@gmail.com>
2024-09-23 22:48:36 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cambio_automatico/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.cambio_automatico;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
